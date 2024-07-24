6eb1b97622feac82f37ea170a072bc6fb19886df
Xi Wang <wangxieric@gmail.com>
2024-07-24 10:34:48 +0100

    Update dataset_processor.py dataload
    add trust_remote_code=True to kilt Wikipedia data load

--- modules/dataset_processor.py
+++ modules/dataset_processor.py
@@ -829,7 +829,7 @@ class KILT100w(Processor):
 
     def process(self):
         hf_name = 'kilt_wikipedia'
-        dataset = datasets.load_dataset(hf_name, num_proc=self.num_proc)[self.split]
+        dataset = datasets.load_dataset(hf_name, num_proc=self.num_proc, trust_remote_code=True)[self.split]
 
         def map_100w(sample, num_words=100):
             wiki_id = sample['wikipedia_id']
