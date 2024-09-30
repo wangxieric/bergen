ca45aba3ac98d15ab770aa6966d814b99bab7e74
Xi Wang <wangxieric@gmail.com>
2024-09-30 14:57:41 +0100

    Create test_qrel.py

---
+++ test_qrel.py
@@ -0,0 +1,17 @@
+import json
+from utils import get_qrel_ranking_filename
+qrels_folder = 'qrels'
+query_dataset_name = 'kilt_nq'
+split = 'dev'
+debug = False
+qrels_file = get_qrel_ranking_filename(qrels_folder, query_dataset_name, split, debug)
+qrel = json.load(open(qrels_file))
+doc_dataset_name = "kilt-100w"
+
+if "doc_dataset_name" in qrel:
+    qrel.pop("doc_dataset_name")
+
+doc_ids = list(qrel.keys())
+for doc_id in doc_ids:
+    if qrel[doc_id].value == 0:
+        print(doc_id)
